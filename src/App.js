6c4f7ce2bf6c62464fa1af0e7e1469f51745ce52
Zumardi Rahman <zumar2014@gmail.com>
2022-10-18 16:16:59 +0700

    Templating-part1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard"; //import komponen
 
 function App() {
   return (
     <div>
-      Zumardirahman
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} /> {/* pada elemen adalah componen yang aka di render */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+//layout membawa parameter children
+const layout = ({children}) => {
+  return (
+    <React.Fragment>
+        <Navbar/>
+        <div className="columns mt-6">
+            <div className="column is-2">
+                <Sidebar/>
+            </div>
+            <div className="column has-background-light">
+                <main>{children}</main>
+            </div>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default layout
