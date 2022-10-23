0b01f92377533b909adce951cfb440f7b67e8897
Zumardi Rahman <zumar2014@gmail.com>
2022-10-23 16:24:05 +0700

    show list data

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+//bisa membuat reduxtor untuk product karna tidak butuh product ini diakses di kompoonen yang lain dan hanya ini
 
 const ProducList = () => {
+  const [products, setProducts] = useState([])
+
+  //manggil metode getProducts didalam useefffect
+  useEffect(()=>{
+    getProducts();
+  },[])
+  //sebuah metod untuk fetcing data
+  const getProducts = async()=>{
+    const response = await axios.get('http://localhost:5000/products')
+
+    //masukan response kedalam statenya
+    setProducts(response.data)
+  }
+  
+  const deleteProduct = async(productId)=>{
+    await axios.delete(`http://localhost:5000/products/${productId}`)
+//panggil lagi metode get prduct agar melihat perobahan interface
+getProducts()
+  }
+  
+
   return (
     <div>
       <h1 className="title">Products</h1>
       <h2 className="subtitle">List of Products</h2>
+      <Link to="/products/add" className="button is-primary mb-2">Add Neew</Link>
       <table className="table is-striped is-fullwidth">
         <thead >
           <tr>
@@ -16,13 +42,18 @@ const ProducList = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          {products.map((product, index)=>( //setiap looping memiliki uniq key
+          <tr key={product.uuid}>
+            <td>{index+1}</td>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+            <td>{product.user.name}</td>
+            <td>
+              <Link to={`/products/edit/${product.uuid}`} className="button is-small is-info">Edit</Link>
+              <button onClick={()=> deleteProduct(product.uuid)} className="button is-small is-danger">Delete</button>
+            </td>
           </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- src/components/users/UserList.jsx
+++ src/components/users/UserList.jsx
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const UserList = () => {
+
+  const [users, setUserss] = useState([])
+
+  //manggil metode getProducts didalam useefffect
+  useEffect(()=>{
+    getUsers();
+  },[])
+  //sebuah metod untuk fetcing data
+  const getUsers = async()=>{
+    const response = await axios.get('http://localhost:5000/users')
+
+    //masukan response kedalam statenya
+    setUserss(response.data)
+  }
+  
+  const deleteUser = async(usertId)=>{
+    await axios.delete(`http://localhost:5000/users/${usertId}`)
+//panggil lagi metode get prduct agar melihat perobahan interface
+getUsers()
+  }
+  
+
   return (
     <div>
       <h1 className="title">Users</h1>
       <h2 className="subtitle">List of Users</h2>
+      <Link to="/users/add" className="button is-primary mb-2">Add New</Link>
       <table className="table is-striped is-fullwidth">
-        <thead >
+        <thead>
           <tr>
             <th>No</th>
             <th>Name</th>
             <th>Email</th>
+            <th>Gender</th>
             <th>Role</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+        {users.map((user, index)=>( //setiap looping memiliki uniq key
+          <tr key={user.uuid}>
+            <td>{index+1}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.gender}</td>
+            <td>{user.role}</td>
+            <td>
+              <Link to={`/users/edit/${user.uuid}`} className="button is-small is-info">Edit</Link>
+              <button onClick={()=> deleteUser(user.uuid)} className="button is-small is-danger">Delete</button>
+            </td>
           </tr>
+          ))}
         </tbody>
       </table>
     </div>
